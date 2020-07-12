8b4924760f099bd0c2a49d75f29a591dd0c0400a
daryabsb <daryabsb@gmail.com>
2020-07-12 16:34:49 +0300

    Worked on budgety first part

--- 4-DOM-pig-game/starter/app-challenge.js
+++ 4-DOM-pig-game/starter/app-challenge.js
@@ -12,49 +12,46 @@ GAME RULES:
 var scores, roundScore, activePlayer, gamePlaying;
 
 init();
+var lastDice;
 
-// scores = [0, 0];
-// roundScore = 0;
-// activePlayer = 0;
-
-// dice = Math.floor(Math.random() * 6) + 1;
-// console.log(dice);
-
-// document.querySelector("#current-" + activePlayer).textContent = dice;
-
-// document.querySelector("#current-" + activePlayer).innerHTML =
-//   "<em>" + dice + "</em>";
-
-// var x = document.querySelector("#score-0").textContent;
-
-// console.log(x);
-
-// document.querySelector(".dice").style.display = "none";
-
-// document.getElementById("score-0").textContent = "0";
-// document.getElementById("score-1").textContent = "0";
-// document.getElementById("current-0").textContent = "0";
-// document.getElementById("current-1").textContent = "0";
-
-// function clickButton() {
-//   alert("Hooray!!");
-// }
 document.querySelector(".btn-roll").addEventListener("click", function () {
   if (gamePlaying) {
     // A random number
-    var dice = Math.floor(Math.random() * 6) + 1;
+    var dice1 = Math.floor(Math.random() * 6) + 1;
+    var dice2 = Math.floor(Math.random() * 6) + 1;
 
-    console.log(dice);
+    // console.log(dice);
     // Display the result
-    var diceDOM = document.querySelector(".dice");
-    diceDOM.style.display = "block";
+    document.getElementById("dice-1").style.display = "block";
+    document.getElementById("dice-2").style.display = "block";
 
-    diceDOM.src = "dice-" + dice + ".png";
+    document.getElementById("dice-1").src = "dice-" + dice1 + ".png";
+    document.getElementById("dice-2").src = "dice-" + dice2 + ".png";
 
     // Update the round score IF the rolled number was not 1
-    if (dice !== 1) {
+    /*
+    if (dice === 6 && lastDice === 6) {
+      // Player loses score
+      scores[activePlayer] = 0;
+      document.querySelector("#score-" + activePlayer).textContent = 0;
+      nextPlayer();
+    } else if (dice !== 1) {
       // Add score
       roundScore += dice;
+      document.querySelector(
+        "#current-" + activePlayer
+      ).textContent = roundScore;
+    } else {
+      // Next player
+
+      nextPlayer();
+    }
+    lastDice = dice;
+    console.log(lastDice);
+    */
+    if (dice1 !== 1 && dice2 !== 1) {
+      // Add score
+      roundScore += dice1 + dice2;
       document.querySelector(
         "#current-" + activePlayer
       ).textContent = roundScore;
@@ -75,10 +72,22 @@ document.querySelector(".btn-hold").addEventListener("click", function () {
     document.querySelector("#score-" + activePlayer).textContent =
       scores[activePlayer];
 
+    var input = document.querySelector(".final-score").value;
+    var winningScore;
+    // Undefined, 0, null or "" are coerced to false
+    // Anything else will be coerced to true
+
+    if (input) {
+      winningScore = input;
+    } else {
+      winningScore = 100;
+    }
+
     // Check if player won the game
-    if (scores[activePlayer] >= 100) {
+    if (scores[activePlayer] >= winningScore) {
       document.querySelector("#name-" + activePlayer).textContent = "Winner!";
-      document.querySelector(".dice").style.display = "none";
+      document.getElementById("dice-1").style.display = "none";
+      document.getElementById("dice-2").style.display = "none";
       document
         .querySelector(".player-" + activePlayer + "-panel")
         .classList.remove("active");
@@ -109,7 +118,8 @@ function nextPlayer() {
   document.querySelector(".player-0-panel").classList.toggle("active");
   document.querySelector(".player-1-panel").classList.toggle("active");
 
-  document.querySelector(".dice").style.display = "none";
+  document.getElementById("dice-1").style.display = "none";
+  document.getElementById("dice-2").style.display = "none";
 }
 
 function init() {
@@ -119,7 +129,8 @@ function init() {
 
   gamePlaying = true;
 
-  document.querySelector(".dice").style.display = "none";
+  document.getElementById("dice-1").style.display = "none";
+  document.getElementById("dice-2").style.display = "none";
 
   document.getElementById("score-0").textContent = "0";
   document.getElementById("score-1").textContent = "0";
---
+++ 6-budgety/starter/app.js
@@ -0,0 +1,155 @@
+// Budget Controller
+var budgetController = (function () {
+  var Expense = function (id, description, value) {
+    this.id = id;
+    this.description = description;
+    this.value = value;
+    this.percentage = -1;
+  };
+
+  var Income = function (id, description, value) {
+    this.id = id;
+    this.description = description;
+    this.value = value;
+  };
+
+  var data = {
+    allItems: {
+      exp: [],
+      inc: []
+    },
+    totals: {
+      exp: 0,
+      inc: 0
+    }
+  };
+  return {
+    addItem: function (type, des, val) {
+      var newItem;
+      // Create new ID
+      if (data.allItems[type].length > 0) {
+        ID = data.allItems[type][data.allItems[type].length - 1].id + 1;
+      } else {
+        ID = 0;
+      }
+
+      // Create new item based on exp or inc
+      if (type === "exp") {
+        newItem = new Expense(ID, des, val);
+      } else {
+        newItem = new Income(ID, des, val);
+      }
+
+      // Push the new element
+      data.allItems[type].push(newItem);
+      return newItem;
+    },
+    testing: function () {
+      console.log(data);
+    }
+  };
+})();
+
+// UI Controller
+var UIController = (function () {
+  var DOMstrings = {
+    inputType: ".add__type",
+    inputDescription: ".add__description",
+    inputValue: ".add__value",
+    inputBtn: ".add__btn",
+    incomeContainer: ".income__list",
+    expensesContainer: ".expenses__list"
+  };
+
+  return {
+    getInput: function () {
+      return {
+        type: document.querySelector(DOMstrings.inputType).value, // Will be either inc or exp
+        description: document.querySelector(DOMstrings.inputDescription).value,
+        value: parseFloat(document.querySelector(DOMstrings.inputValue).value)
+      };
+    },
+    addListItem: function (obj, type) {
+      var html, newHtml, element;
+      // Create HTM String with placeholder text
+
+      if (type === "inc") {
+        element = DOMstrings.incomeContainer;
+        html = `<div class="item clearfix" id="income-%id%"><div class="item__description">%description%</div><div class="right clearfix"><div class="item__value">%value%</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>`;
+      } else if (type === "exp") {
+        element = DOMstrings.expensesContainer;
+        html = `<div class="item clearfix" id="expense-%id%"><div class="item__description">%description%</div><div class="right clearfix"><div class="item__value">%value%</div><div class="item__percentage">21%</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div>`;
+      }
+
+      // Replace Placeholder text with some actual data
+      newHtml = html.replace("%id%", obj.id);
+      newHtml = newHtml.replace("%description%", obj.description);
+      newHtml = newHtml.replace("%value%", obj.value);
+
+      // Insert HTM into the DOM
+      document.querySelector(element).insertAdjacentHTML("beforeend", newHtml);
+    },
+    clearFields: function () {
+      var fields, fieldsArr;
+
+      fields = document.querySelectorAll(
+        DOMstrings.inputDescription + ", " + DOMstrings.inputValue
+      );
+
+      fieldsArr = Array.prototype.slice.call(fields);
+      fieldsArr.forEach(el => (el.value = ""));
+      fieldsArr[0].focus();
+    },
+    getDOMstrings: function () {
+      return DOMstrings;
+    }
+  };
+})();
+
+// Global App Controller
+var controller = (function (budCtrl, UICtrl) {
+  var setupEventListeners = function () {
+    var DOM = UICtrl.getDOMstrings();
+
+    document.querySelector(DOM.inputBtn).addEventListener("click", ctrlAddItem);
+
+    document.addEventListener("keypress", function (event) {
+      if (event.keyCode === 13 || event.which === 13) {
+        ctrlAddItem();
+      }
+    });
+  };
+
+  var ctrlAddItem = function () {
+    var input, newItem;
+
+    // 1. Get the field input data
+    input = UICtrl.getInput();
+
+    // 2. Add the item to the budget controller
+    newItem = budgetController.addItem(
+      input.type,
+      input.description,
+      input.value
+    );
+
+    // 3. Add the item to the UI
+    UIController.addListItem(newItem, input.type);
+
+    // 4. Clear the fields
+    UIController.clearFields();
+
+    // 5. Calculate and update budget
+
+    // 6. Calculate and update percentages
+  };
+
+  return {
+    init: function () {
+      console.log("Application has started.");
+
+      setupEventListeners();
+    }
+  };
+})(budgetController, UIController);
+controller.init();
